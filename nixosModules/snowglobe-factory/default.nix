5d64ba6f206dd9e9c47c8cbe558105f954c1135f
EarthGman <earthgman@noreply.codeberg.org>
2026-09-11 20:00:21 -0500

    add selectdefaultapplication-qt6

--- nixosModules/snowglobe-factory/default.nix
+++ nixosModules/snowglobe-factory/default.nix
@@ -209,6 +209,8 @@ in
       jq.enable = slib.setDefault true;
       # better top
       btop.enable = slib.setDefault true;
+      # improved selectdefaultapplication
+      selectdefaultapplication.package = slib.setDefault pkgs.selectdefaultapplication-qt6;
       # fuzzy finder and manager for systemd units
       sysz.enable = slib.setDefault true;
       # wrapper script for nixos-rebuild
